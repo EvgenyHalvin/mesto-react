bca9911f52e4258be14bd5d9c551fddda40e96b5
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2021-05-27 20:54:12 +0200

    the first react app

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import api from '../utils/Api';
-import Card from './Card';
+import React from "react";
+import api from "../utils/Api";
+import Card from "./Card";
 
 function Main(props) {
   const [userName, setUserName] = React.useState();
@@ -8,51 +8,52 @@ function Main(props) {
   const [userAvatar, setUserAvatar] = React.useState();
   const [cards, setCards] = React.useState([]);
 
-
   React.useEffect(() => {
-    api.getUserProfile()
-      .then((data) => {
-        setUserAvatar(data.avatar)
-        setUserName(data.name)
-        setuserDescription(data.about)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [userName, userDescription, userAvatar]);
-
-  React.useEffect(() => {
-    api.getInitialCards()
-      .then((res) => {
-        setCards(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, []);
+    Promise.all([api.getUserProfile(), api.getInitialCards()]).then(
+      ([data, res]) => {
+        setUserAvatar(data.avatar);
+        setUserName(data.name);
+        setuserDescription(data.about);
+        setCards(res);
+      }
+    );
+  });
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-wrap">
-          <div className="profile__avatar"
+          <div
+            className="profile__avatar"
             style={{ backgroundImage: `url(${userAvatar})` }}
           ></div>
-          <div className="profile__avatar-layout" onClick={props.onEditAvatar}></div>
+          <div
+            className="profile__avatar-layout"
+            onClick={props.onEditAvatar}
+          ></div>
         </div>
         <div className="profile__profile-into">
           <div className="profile__wrap">
             <h1 className="profile__name">{userName}</h1>
-            <button type="button" className="profile__edit-button" onClick={props.onEditProfile}></button>
+            <button
+              type="button"
+              className="profile__edit-button"
+              onClick={props.onEditProfile}
+            ></button>
           </div>
           <p className="profile__description">{userDescription}</p>
         </div>
-        <button type="button" className="profile__add-button" onClick={props.onAddPlace}></button>
+        <button
+          type="button"
+          className="profile__add-button"
+          onClick={props.onAddPlace}
+        ></button>
       </section>
 
       <section className="elements">
-        {cards.map(item =>
-          (<Card key={item._id} card={item} onCardClick={props.onCardClick} />))}
+        {cards.map((item) => (
+          <Card key={item._id} card={item} onCardClick={props.onCardClick} />
+        ))}
       </section>
     </main>
   );
